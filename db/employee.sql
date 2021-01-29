ee0badd125f38a80d78c7635e88f3dd69d7590ab
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-01-28 21:09:19 -0800

    Fix typos/errors in schema

--- db/employee.sql
+++ db/employee.sql
@@ -6,8 +6,7 @@ USE employee_DB;
 CREATE TABLE department (
     id INT PRIMARY KEY AUTO_INCREMENT ,
     -- UNIQUE added so that no duplicate names can be added
-    name VARCHAR(30) UNIQUE,
-    
+    name VARCHAR(30) UNIQUE
 );
 
 CREATE TABLE role (
@@ -17,8 +16,8 @@ CREATE TABLE role (
     department_id INT,
     -- Create foreign key that references department id
     -- ON delete added because if a deparment get deleted, the role no longer exist.
-    CONSTRAINT fk_department FOREIGN KEY (deparment_id) REFERENCES department (id) ON DELETE CASCADE
-)
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
+);
 
 CREATE TABLE employee (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -27,10 +26,9 @@ CREATE TABLE employee (
     role_id INT,
     -- Foreign key that references role id
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE,
-    
+
     -- Foreign key that references employee id
     -- Set to NULL because an employee can exist without a manager.
     manager_id INT,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE SET NULL,
-
-)
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE SET NULL
+);
